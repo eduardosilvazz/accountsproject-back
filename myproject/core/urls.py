10eefad2a5a34a73567fdf33cf2ad2b71d4f7a02
Eduardo Carvalho <eduardocgsilva1@gmail.com>
2024-07-10 12:59:20 -0300

    Testing the Django API

---
+++ myproject/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ProductList, ProductDetail, CategoryList
+
+urlpatterns = [
+    path('products/', ProductList.as_view(), name='product_list'),
+    path('products/<int:pk>/', ProductDetail.as_view(), name='product_detail'),
+    path('categories/', CategoryList.as_view(), name='category_list'),
+]
